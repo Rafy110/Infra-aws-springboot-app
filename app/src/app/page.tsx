4e455fe5f3349f3c7bb66612d5c8d4525e01bd82
Rafy110 <rafiikhann69@gmail.com>
2025-12-05 12:41:54 -0800

    To productionnn

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="container">
-      <h1>Welcome to Next.js Frontend</h1>
+      <h1>Welcome to Next.js Frontend appppp</h1>
       <p>This application is running on AWS ECS Fargate</p>
       <div className="info">
         <p>Environment: {process.env.NEXT_PUBLIC_ENV || 'development'}</p>
